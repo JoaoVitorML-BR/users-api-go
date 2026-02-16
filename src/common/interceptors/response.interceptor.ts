bcc22fb615c05fbcde076821b495d3773516ce4e
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-02-16 20:16:53 -0300

    feat: implements find all users with response interceptor global

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -11,12 +11,25 @@ import { Observable } from 'rxjs';
 export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         return next.handle().pipe(
-            map((data) => ({
-                statusCode: context.switchToHttp().getResponse().statusCode,
-                status: true,
-                message: 'Request successful',
-                data,
-            })),
+            map((data) => {
+                console.log('ResponseInterceptor data:', data);
+                if (
+                    data &&
+                    typeof data === 'object' &&
+                    'statusCode' in data &&
+                    'status' in data &&
+                    'message' in data &&
+                    'data' in data
+                ) {
+                    return data;
+                }
+                return {
+                    statusCode: context.switchToHttp().getResponse().statusCode,
+                    status: true,
+                    message: 'Request successful',
+                    data,
+                };
+            }),
         );
     }
 }
