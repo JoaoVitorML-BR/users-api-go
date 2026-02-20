2b22c4448b12e953a55c8bc5224b73da3c389284
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-02-20 11:10:05 -0300

    feat: add JWT guard and role-based authorization to user endpoints

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,18 +1,33 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { FindAllUsersUseCase } from "./use-cases/find-all-users.use-case";
-import { CreateUserDTO } from "./dto/create-users.dto";
+import { CreateUserAdminDTO, CreateUserDTO } from "./dto/create-users.dto";
 import { CreateUsersUseCase } from "./use-cases/create-users.use-case";
+import { CreateUsersAdminUseCase } from "./use-cases/create-users-admin.use-case";
+import { JwtGuard } from "../auth/guards/jwt.guard";
+import { RolesGuard } from "../auth/guards/roles.guard";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { ROLE } from "./user.entity";
 
 @Controller('users')
 export class UserController {
     constructor(
         private readonly findAllUsersUseCase: FindAllUsersUseCase,
-        private readonly createUsersUseCase: CreateUsersUseCase
+        private readonly createUsersUseCase: CreateUsersUseCase,
+        private readonly createAdminUseCase: CreateUsersAdminUseCase
     ) { }
 
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles(ROLE.ADMIN_MASTER, ROLE.ADMIN)
     @Get()
     async findAll() {
         return this.findAllUsersUseCase.findAll();
+    }
+
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles(ROLE.ADMIN_MASTER)
+    @Post('create-admin')
+    async createAdmin(@Body() Data: CreateUserAdminDTO) {
+        return this.createAdminUseCase.createAdmin(Data);
     }
 
     @Post()
